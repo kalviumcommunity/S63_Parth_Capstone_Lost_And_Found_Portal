505251ed3e344f9163514576178fff64c463c86f
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2025-04-11 10:57:44 +0530

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,6 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Api is running...');
-});
-
-
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 })
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 const Login = () => {
-    return <h1>LoginPage</h1>
+    return <h1>Login Page</h1>
 };
 
 export default Login;
