0e18ff4099be35a23d181037240d9b3340613e34
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2025-04-18 12:24:59 +0530

    tailwind installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
-import AppRouter from "./Router";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Guidelines from './pages/Guidelines';
+// import Disclaimer from './pages/Disclaimer';
+// import Donate from './pages/Donate';
+// import Contact from './pages/Contact';
+// import Careers from './pages/Careers';
+// import MakePayment from './pages/MakePayment';
+import Login from './pages/Login';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/guidelines" element={<Guidelines />} /> */}
+        {/* <Route path="/disclaimer" element={<Disclaimer />} /> */}
+        {/* <Route path="/donate" element={<Donate />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        {/* <Route path="/careers" element={<Careers />} /> */}
+        {/* <Route path="/make-payment" element={<MakePayment />} /> */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Include your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/TS/JSX/TSX files in src
+  ],
+  theme: {
+    extend: {}, // You can customize theme colors, fonts, etc. here later
+  },
+  plugins: [],
+}
