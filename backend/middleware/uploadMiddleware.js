5a4db9a627811407f5453801c4ccaa4f6137dafc
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2025-04-19 13:25:50 +0530

    middleware upload fuctionality code

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowed = /jpeg|jpg|png/;
+  const ext = allowed.test(path.extname(file.originalname).toLowerCase());
+  const mime = allowed.test(file.mimetype);
+  if (ext && mime) cb(null, true);
+  else cb("Only images allowed");
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
